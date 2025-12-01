0a7fdcb8af3e44f8aae3a21d4dba584fd82dd013
Cryoonic <albertalexander2004@gmail.com>
2025-12-01 11:26:54 +0700

    Commit No. 3 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
 import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.Flow
 
 class MovieRepository(private val movieService: MovieService) {
     private val apiKey = "d2bf2879c67c244a02ee7c945edecbad"
@@ -16,15 +20,11 @@ class MovieRepository(private val movieService: MovieService) {
     val error: LiveData<String>
         get() = errorLiveData
 
-    suspend fun fetchMovies(){
-        try{
-            val popularMovies = movieService.getPopularMovies(apiKey)
-            movieLiveData.postValue(popularMovies.results)
-        } catch (exception: Exception){
-            errorLiveData.postValue(
-                "An error occured: ${exception.message}"
-            )
-        }
+    fun fetchMovies(): Flow<List<Movie>>{
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
     }
+
 
 }
